bd04ffdb443fa439a8262f89bf506115ed6f0e7e
Mr-Fang-VLSI <donghao@DESKTOP-02GHJ2K.localdomain>
2022-10-11 12:48:25 -0500

    u

--- run_one.sh
+++ run_one.sh
@@ -1,8 +1,9 @@
 sudo bash make.sh | tee "complie_log.txt"
 cd test
-export NUM=8
-export FASTWL=1
+# export NUM=8
+# export FASTWL=1
 
-replace  <  gcd_nontd_test.tcl
+# replace  <  gcd_nontd_test.tcl
+replace  <  asap7.tcl
 
 cd ..
